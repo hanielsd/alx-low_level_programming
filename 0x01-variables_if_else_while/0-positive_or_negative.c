d45cb2e162d098bcd1c726235cdc75283f6420dc
Halefom Alemu <hannelalex12@gmail.com>
2023-02-17 08:37:05 -0800

    checking positivity of a random number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/*
- * main - an entry point 
- * Description - This code checks the positivity of a random number and then it prints a message accordingly.
+#include <stdio.h>
+
+/**
+ * main - Entry point
  *
- * Retrun: int
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -13,14 +13,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n>0)
-		printf("%d is positive", n);
-	else if (n<0)
-		printf("%d is negative", n);
-	else
-		printf("%d is zero", n);
-	printf("\n");
 
- 	return (0);
+	if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is positive\n", n);
+	}
+	return (0);
 }
